bb9f35be481d6f62c5b4c2acdb22e2ce90eb8b4a
Arman Zarif <arman1zarif@yahoo.com>
2024-02-06 19:36:52 +0330

    feat: Added module-level docstring to company_info.py and updated pylint configuration

--- server/company_info.py
+++ server/company_info.py
@@ -1,6 +1,17 @@
+"""
+This module provides functions to retrieve and manipulate company information.
+
+Dictionary:
+    COMPANY_INFO dictionary for containing the info
+
+Functions:
+  No function yet for returning the info
+"""
 # company_info.py
 
 COMPANY_INFO = {
+    """ This is the information of the company """
+    
     'address': '123 Main St, Cityville, Country',
     'telephone': '+1 (555) 123-45670',
     # Add other company information as needed
