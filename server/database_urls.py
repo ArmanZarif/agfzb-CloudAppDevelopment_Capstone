263651faa89a8ccff8d5d8a67095ad8b30b1faab
arman1zarif@yahoo.com <you@example.com>
2024-02-18 05:51:28 -0500

    Containerize Django Application

--- server/database_urls.py
+++ server/database_urls.py
@@ -10,7 +10,7 @@ DATABASE_URLS = {
     # """ This is the information of the URLS """
     
     'dealerships': 'https://arman1zarif-3000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
-    # 'reviews': 'https://arman1zarif-5000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
-    'reviews': 'http://127.0.0.1:5000',
+    'reviews': 'https://arman1zarif-5000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    # 'reviews': 'http://127.0.0.1:5000',
     # Add other company information as needed
 }
